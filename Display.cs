644ae5847c5fbd36c4cdb4d2d8af079a7776a0ac
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2023-10-11 19:45:00 +0300

    Add files via upload

---
+++ Display.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider _healthBar;
+    [SerializeField] private Image _buttonAddHealth;
+    [SerializeField] private Image _buttonRemoveHealth;
+
+    private Coroutine _coroutine;
+    private int _health = 10;
+
+    public void AddHealth()
+    {
+        float targetValue = _healthBar.value + _health;
+
+        _coroutine = StartCoroutine(ChangeHealthBar(targetValue));
+    }
+
+    public void RemoveHealth()
+    {
+        float targetValue = _healthBar.value - _health;
+
+        _coroutine = StartCoroutine(ChangeHealthBar(targetValue));     
+    }
+
+    private IEnumerator TransformHealthLevel(float targetValue)
+    {
+        bool isWork = true;
+
+        while (isWork)
+        {
+            _healthBar.value = Mathf.MoveTowards(_healthBar.value, targetValue, Time.deltaTime * _health);
+
+            if(_healthBar.value == targetValue)
+                isWork = false;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator ChangeHealthBar(float targetValue)
+    {
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+
+        yield return StartCoroutine(TransformHealthLevel(targetValue));
+
+        if (_healthBar.value == targetValue)
+            StopCoroutine(_coroutine);
+    }
+}
