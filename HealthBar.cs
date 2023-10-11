3be5c8276b73f4ed646213c1ab930ac45ea3a7b6
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2023-10-11 22:33:07 +0300

    Update HealthBar.cs

--- HealthBar.cs
+++ HealthBar.cs
@@ -1,53 +1,22 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class Display : MonoBehaviour
+[RequireComponent(typeof(Display))]
+public class HealthBar : MonoBehaviour
 {
-    [SerializeField] private Slider _healthBar;
-    [SerializeField] private Button _buttonAddHealth;
-    [SerializeField] private Button _buttonRemoveHealth;
+    private Display _display;
 
-    private Coroutine _coroutine;
-    private int _health = 10;
-
-    public void AddHealth()
+    private void Start()
     {
-        float targetValue = _healthBar.value + _health;
-
-        _coroutine = StartCoroutine(ChangeHealthBar(targetValue));
+        _display = GetComponent<Display>();
     }
 
-    public void RemoveHealth()
+    public void AppendHealth()
     {
-        float targetValue = _healthBar.value - _health;
-
-        _coroutine = StartCoroutine(ChangeHealthBar(targetValue));
+        _display.AddHealth();
     }
 
-    private IEnumerator TransformHealthLevel(float targetValue)
+    public void DeleteHealth()
     {
-        bool isWork = true;
-
-        while (isWork)
-        {
-            _healthBar.value = Mathf.MoveTowards(_healthBar.value, targetValue, Time.deltaTime * _health);
-
-            if (_healthBar.value == targetValue)
-                isWork = false;
-
-            yield return null;
-        }
-    }
-
-    private IEnumerator ChangeHealthBar(float targetValue)
-    {
-        if (_coroutine != null)
-            StopCoroutine(_coroutine);
-
-        yield return StartCoroutine(TransformHealthLevel(targetValue));
-
-        if (_healthBar.value == targetValue)
-            StopCoroutine(_coroutine);
+        _display.RemoveHealth();
     }
 }
