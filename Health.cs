c0dc2bf92fd2b7c04879122eb5b0583985946a34
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2023-10-17 14:55:12 +0300

    Update Health.cs

--- Health.cs
+++ Health.cs
@@ -1,22 +1,42 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] private Slider _healthBar;
+    [SerializeField] private Image _buttonAddHealth;
+    [SerializeField] private Image _buttonRemoveHealth;
 
     private Coroutine _coroutine;
     private int _health = 10;
 
-    public void IncreaseWellness()
+    public event UnityAction<float> HealthChanged;
+
+    private void Start()
+    {
+        HealthChanged?.Invoke(_healthBar.value);
+    }
+
+    public void AddHealth()
+    {
+        IncreaseWellness();
+    }
+
+    public void RemoveHealth()
+    {
+        ReduceWellness();
+    }
+
+    private void IncreaseWellness()
     {
         float targetValue = _healthBar.value + _health;
 
         _coroutine = StartCoroutine(ChangeHealthBar(targetValue));
     }
 
-    public void ReduceWellness()
+    private void ReduceWellness()
     {
         float targetValue = _healthBar.value - _health;
 
