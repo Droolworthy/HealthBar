af49ec1af73bac6067f30d5008fb884d0a3c46ac
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2023-10-20 16:03:27 +0300

    Update Health.cs

--- Health.cs
+++ Health.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -8,18 +7,10 @@ public class Health : MonoBehaviour
     [SerializeField] private Slider _healthBar;
     [SerializeField] private Image _buttonAddHealth;
     [SerializeField] private Image _buttonRemoveHealth;
-    [SerializeField] private HealthBar _healthBand;
 
-    private int _health = 10;
-    private Coroutine _coroutine;
+    private readonly int _health = 10;
 
-    public Coroutine Сoroutine => _coroutine;
     public event UnityAction<float> HealthChanged;
-
-    private void Start()
-    {
-        HealthChanged?.Invoke(_healthBar.value);
-    }
 
     public void AddHealth()
     {
@@ -31,32 +22,20 @@ public class Health : MonoBehaviour
         ReduceWellness();
     }
 
-    public IEnumerator TransformWellnessLevel(float targetValue)
-    {
-        bool isWork = true;
-
-        while (isWork)
-        {
-            _healthBar.value = Mathf.MoveTowards(_healthBar.value, targetValue, Time.deltaTime * _health);
-
-            if (_healthBar.value == targetValue)
-                isWork = false;
-
-            yield return null;
-        }
-    }
-
     private void IncreaseWellness()
     {
-        float targetValue = _healthBar.value + _health;
-
-        _coroutine = StartCoroutine(_healthBand.ChangeHealthBar(targetValue));
+        ChangeWellness(_health);
     }
 
     private void ReduceWellness()
     {
-        float targetValue = _healthBar.value - _health;
+        ChangeWellness(-_health);
+    }
 
-        _coroutine = StartCoroutine(_healthBand.ChangeHealthBar(targetValue));
-    }  
+    private void ChangeWellness(float health) 
+    {
+        float targetValue = _healthBar.value + health;
+
+        HealthChanged?.Invoke(targetValue);
+    }
 }
