90e37eeb45f4e0a160968f8c349874416d7f6dd5
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2023-10-20 16:04:18 +0300

    Update HealthBar.cs

--- HealthBar.cs
+++ HealthBar.cs
@@ -8,16 +8,7 @@ public class HealthBar : MonoBehaviour
     [SerializeField] private Slider _healthBar;
     [SerializeField] private Health _health;
 
-    public IEnumerator ChangeHealthBar(float targetValue)
-    {
-        if (_health.Сoroutine != null)
-            StopCoroutine(_health.Сoroutine);
-
-        yield return StartCoroutine(_health.TransformWellnessLevel(targetValue));
-
-        if (_healthBar.value == targetValue)
-            StopCoroutine(_health.Сoroutine);
-    }
+    private Coroutine _coroutine;
 
     private void OnEnable()
     {
@@ -31,6 +22,34 @@ public class HealthBar : MonoBehaviour
 
     private void OnHealthChanged(float health)
     {
-        _healthBar.value = health;
+        _coroutine = StartCoroutine(ChangeWellnessBand(health));
+    }
+
+    private IEnumerator ChangeWellnessBand(float targetValue) 
+    {
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+
+        yield return StartCoroutine(TransformWellnessLevel(targetValue));
+
+        if (_healthBar.value == targetValue)
+            StopCoroutine(_coroutine);
+    }
+
+    private IEnumerator TransformWellnessLevel(float targetValue)
+    {
+        bool isWork = true;
+
+        int health = 10;
+
+        while (isWork)
+        {
+            _healthBar.value = Mathf.MoveTowards(_healthBar.value, targetValue, Time.deltaTime * health);
+
+            if (_healthBar.value == targetValue)
+                isWork = false;
+
+            yield return null;
+        }
     }
 }
